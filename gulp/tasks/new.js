3753c3cf0739cd4d5d6090ba7260f9703656533b
Nick <theb0ffee@gmail.com>
2020-08-17 17:57:13 +0300

    new file gulp add

--- gulp/tasks/new.js
+++ gulp/tasks/new.js
@@ -14,24 +14,23 @@ const {argv} = require('yargs')
 const content = ''
 
 $.gulp.task('n', () => {
-	// let a = argv.name
-	// console.log(a)
-	// $.fs.writeFile('./build/{argv.name}.txt', content)
-	// if (argv.ships > 3 && argv.distance < 53.5) {
-	//   console.log('Plunder more riffiwobbles!')
-	//   console.log(argv.ships)
-	// } else {
-	//   console.log('Retreat from the xupptumblers!')
-	// }
+
 		let a = argv.n
+
 		$.fs.writeFile(`./dev/pug/modules/${a}.pug`, content, (err) => {
 	  	if (err) {
 		    console.error(err)
 		    return
 		  }
 		})
+		$.fs.mkdir(`./dev/static/stylus/modules/${a}`, (err) => {
+				if (err) {
+			    console.error(err)
+			    return
+			  }
+			})
 
-		$.fs.writeFile(`./dev/static/stylus/${a}/${a}.styl`, content, (err) => {
+		$.fs.writeFile(`./dev/static/stylus/modules/${a}/${a}.styl`, content, (err) => {
 	  	if (err) {
 		    console.error(err)
 		    return
@@ -44,29 +43,6 @@ $.gulp.task('n', () => {
 		    return
 		  }
 		})
-		window.clipboardData.setData("Text", text);
  
 })
-
- 
-// require('yargs') // eslint-disable-line
-//   .command('serve [port]', 'start the server', (yargs) => {
-//     yargs
-//       .positional('port', {
-//         describe: 'port to bind on',
-//         default: 5000
-//       })
-//   }, (argv) => {
-//     if (argv.verbose) console.info(`start server on :${argv.port}`)
-//     serve(argv.port)
-//   })
-//   .option('verbose', {
-//     alias: 'v',
-//     type: 'boolean',
-//     description: 'Run with verbose logging'
-//   })
-//   .argv
-
-
-
 }
